7b2f0d6799a2d8209af9bdb199fd336de5cec80a
Jean <jeansscastro@gmail.com>
2025-07-07 13:06:28 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/zjeanero.png" />
-    <Titulo fontSize={20}>Jean Secco</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Jean Secco</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        zjeanero
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Front-End Developer
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
